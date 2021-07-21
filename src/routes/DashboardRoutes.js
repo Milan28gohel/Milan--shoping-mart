ca3844de58d977aa7cff195108dac43c9ac9cd24
MAC <mac@DREAMSs-iMac-Pro.local>
2021-07-21 15:35:20 +0530

    product display

--- src/routes/DashboardRoutes.js
+++ src/routes/DashboardRoutes.js
@@ -1,68 +1,66 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import PropTypes from "prop-types";
+
 import * as routes from "../lib/constants";
+
 import { ItemList, Product } from "../component";
 
 class DashboardRoutes extends Component {
-    render(){
-        return(
-            <div>
-                <Switch>
-                    <Route 
-                    exact 
-                    path={routes.ROOT_ROUTE}
-                    component={this.handleItemList}
-                     />
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route
+            exact
+            path={routes.ROOT_ROUTE}
+            component={this.handleItemList}
+          />
+          <Route
+            exact
+            path={routes.ITEMS_ROUTE}
+            component={this.handleItemList}
+          />
+          <Route exact path={routes.ITEM_ROUTE} component={Product} />
+          <Route
+            exact
+            path={routes.CARTS_ROUTE}
+            component={this.handleCartList}
+          />
+        </Switch>
+      </div>
+    );
+  }
 
-                    <Route 
-                    exact
-                    path={routes.ITEMS_ROUTE}
-                    component={this.handleItemList}
-                    /> 
+  handleItemList = () => {
+    const { items, handleAddToCart } = this.props;
+    return (
+      <ItemList
+        items={items}
+        handleAddToCart={handleAddToCart}
+        isCart={false}
+      />
+    );
+  };
 
-                    <Route exact path={routes.ITEMS_ROUTE} component={Product} />
+  handleCartList = () => {
+    const { carts, handleRemoveToCart } = this.props;
+    return (
+      <ItemList items={carts} handleRemoveToCart={handleRemoveToCart} isCart />
+    );
+  };
 
-                    <Route
-                    exact
-                    path={routes.CARTS_ROUTE}
-                    component={this.handleCartList}
-                    />
-                </Switch>
-            </div>
-        );
-    }
-    
-    handleItemList=()=>{
-        const{items,handleAddtoCart} = this.props;
-        console.log(items);
-        return(
-            <ItemList 
-              items={items}
-              handleAddtoCart={handleAddtoCart}
-              isCart={false}
-            />
-        );
-    };
-
-    handleCartList=()=>{
-        const{carts,handleRemoveToCart}=this.props;
-        return(
-            <ItemList items={carts} handleRemoveToCart={handleRemoveToCart} isCart />
-        );
-    };
-
-    handleProduct=()=>{
-        const{handleAddtoCart}=this.props;
-        return<Product handleAddtoCart={handleAddtoCart} />;
-    };
+  handleProduct = () => {
+    const { handleAddToCart } = this.props;
+    return <Product handleAddToCart={handleAddToCart} />;
+  };
 }
 
-DashboardRoutes.propTypes ={
-    items:PropTypes.any,
-    handleAddtoCart:PropTypes.func.isRequired,
-    handleRemoveToCart:PropTypes.func.isRequired,
-    carts:PropTypes.array
+DashboardRoutes.propTypes = {
+  items: PropTypes.any,
+  handleAddToCart: PropTypes.func.isRequired,
+  handleRemoveToCart: PropTypes.func.isRequired,
+  carts: PropTypes.array
 };
 
-export default  DashboardRoutes;
+export default DashboardRoutes;
